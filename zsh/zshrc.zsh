a7ef909e4f10707bd73f7e29c1b17590f26a8048
wit-l <witty.ysl@gmail.com>
2025-09-15 09:11:57 +0800

    Set the completion menu of Aloxaf/fzf-tab to preview the content of files, folders and pictures.

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -30,13 +30,25 @@ bindkey '^[b' backward-word
 
 # == fzf-tab
 zstyle ':fzf-tab:complete:_zlua:*' query-string input
+# preview of full commandline arguments
+# give a preview of commandline arguments when completing `kill|ps`
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm,cmd -w -w"
-zstyle ':fzf-tab:complete:kill:argument-rest' fzf-preview 'ps --pid=$word -o cmd --no-headers -w -w'
-zstyle ':fzf-tab:complete:kill:argument-rest' fzf-flags '--preview-window=down:3:wrap'
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview \
+  '[[ $group == "[process ID]" ]] && ps --pid=$word -o cmd --no-headers -w -w'
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags '--preview-window=down:3:wrap'
 zstyle ':fzf-tab:complete:kill:*' popup-pad 0 3
-zstyle ':fzf-tab:complete:cd:*' popup-pad 30 0
-zstyle ":fzf-tab:*" fzf-flags --color=bg+:23
+
+# custom fzf flags
+# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+zstyle ':fzf-tab:*' fzf-flags --height='80%' --color=bg+:23,border:#cceeff \
+  --border=rounded -e --ansi --preview-window='65%'
+# enable tmux popup for fzf to show results
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+# set the lines number of fzf's prompt occupied
+zstyle ':fzf-tab:*' fzf-pad 4
+# bind <space> to accept the selected item
+zstyle ':fzf-tab:*' fzf-bindings 'space:accept'
+
 zstyle ':completion:*' file-sort modification
 zstyle ':completion:*:eza' sort false
 zstyle ':completion:files' sort false
@@ -51,12 +63,35 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu no
 # preview directory's content with eza when completing cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 -a --color=always $realpath'
+# set the right and bottom padding of the popup window
+zstyle ':fzf-tab:complete:cd:*' popup-pad 30 0
+# set the minimum size of the popup window in tmux
+# apply to all command
+zstyle ':fzf-tab:*' popup-min-size 120 50
+# set minimal height of fzf's prompt
+# zstyle ':fzf-tab:*' fzf-min-height 80
+# only apply to 'diff'
+zstyle ':fzf-tab:complete:diff:*' popup-min-size 80 16
 # switch group using `<` and `>`
 zstyle ':fzf-tab:*' switch-group '<' '>'
 # set case insensitive when change directory with cd
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
 zstyle ':completion:*:*:docker:*' option-stacking yes
 zstyle ':completion:*:*:docker-*:*' option-stacking yes
+# show systemd unit status
+zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+
+# show file contents
+# zstyle ':fzf-tab:complete:*:*' fzf-preview 'bat --style=numbers --color=always ${(Q)realpath}'
+zstyle ':fzf-tab:complete:(cd|cat|v|vim|nvim|chafa|viu):*' fzf-preview 'less ${(Q)realpath}'
+export LESSOPEN='|~/.config/.lessfilter %s'
+# To disable or override preview for command options and subcommands, use
+zstyle ':fzf-tab:complete:*:options' fzf-preview
+zstyle ':fzf-tab:complete:*:argument-1' fzf-preview
+
+# environment variable
+zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' \
+	fzf-preview 'echo ${(P)word}'
 
 compdef _mv advmv
 compdef _cp advcp
