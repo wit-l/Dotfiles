1e0ae16cfea1b6d28cd28f90cfb3143abef18c9d
wit-l <witty.ysl@gmail.com>
2025-09-27 18:52:39 +0800

    Delete themes from marketplace

--- windows/pwsh/CurrentUserCurrentHost.ps1
+++ windows/pwsh/CurrentUserCurrentHost.ps1
@@ -1,21 +1,26 @@
 # load completion function
 $cmp_paths = @(
-    # "D:\Software\zoxide\completions\_zoxide.ps1",
-    # "D:\Software\MsysSoftware\BurntSushi---ripgrep\ripgrep\complete\_rg.ps1",
-    # "D:\Software\MsysSoftware\sharkdp---bat\bat\autocomplete\_bat.ps1",
-    # "D:\Software\MsysSoftware\sharkdp---fd\fd\autocomplete\fd.ps1",
-    # "D:\Users\witty\Documents\PowerShell\Completions\fnm.ps1",
-    "D:\Users\witty\Documents\PowerShell\Scripts"
+  # "D:\Software\zoxide\completions\_zoxide.ps1",
+  # "D:\Software\MsysSoftware\BurntSushi---ripgrep\ripgrep\complete\_rg.ps1",
+  # "D:\Software\MsysSoftware\sharkdp---bat\bat\autocomplete\_bat.ps1",
+  # "D:\Software\MsysSoftware\sharkdp---fd\fd\autocomplete\fd.ps1",
+  # "D:\Users\witty\Documents\PowerShell\Completions\fnm.ps1",
+  "D:\Users\witty\Documents\PowerShell\Scripts"
 )
 $env:EDITOR="nvim"
-foreach ($cmp_path in $cmp_paths) {
-    if (Test-Path -Path $cmp_path -PathType Container) { # 文件夹则使其中的ps1均生效
-        Get-ChildItem -Path $cmp_path -Filter *.ps1 | ForEach-Object {
-            . $_.FullName
-        }
-    }elseif (Test-Path -Path $cmp_path) {
-        . $cmp_path
+$env:KOMOREBI_CONFIG_HOME = "$env:USERPROFILE\.config\komorebi"
+$env:KOMOREBI_AHK_EXE = "AutoHotkey64.exe"
+foreach ($cmp_path in $cmp_paths)
+{
+  if (Test-Path -Path $cmp_path -PathType Container)
+  { # 文件夹则使其中的ps1均生效
+    Get-ChildItem -Path $cmp_path -Filter *.ps1 | ForEach-Object {
+      . $_.FullName
     }
+  } elseif (Test-Path -Path $cmp_path)
+  {
+    . $cmp_path
+  }
 }
 Remove-Variable -Name cmp_paths,cmp_path
 
