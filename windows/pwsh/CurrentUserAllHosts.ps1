45f3d0ab00b5a3c35266535dc04854ed51e289f3
wit-l <witty.ysl@gmail.com>
2025-10-10 16:23:34 +0800

    feat(pwsh): Move pwsh config and change init script of python env

--- windows/pwsh/CurrentUserAllHosts.ps1
+++ windows/pwsh/CurrentUserAllHosts.ps1
@@ -1,7 +1,19 @@
-
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+$PSStyle.FileInfo.Directory="`e[34;1m"
+starship init powershell | Invoke-Expression
+# 初始化zoxide
+zoxide init powershell | Out-String | Invoke-Expression
+# 初始化fnm
+fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+#------------------------------- Set Hot-keys BEGIN -------------------------------
+# 设置预测文本来源为历史记录
+Set-PSReadLineOption -PredictionSource History -HistorySearchCursorMovesToEnd -EditMode Emacs -PredictionViewStyle InlineView -BellStyle None 
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+#------------------------------- Set Hot-keys END -------------------------------
 #region mamba initialize
 # !! Contents within this block are managed by 'mamba shell init' !!
-$Env:MAMBA_ROOT_PREFIX = "D:\Software\miniconda3"
-$Env:MAMBA_EXE = "D:\Software\miniconda3\Library\bin\mamba.exe"
+$Env:MAMBA_ROOT_PREFIX = "D:\Software\miniforge3"
+$Env:MAMBA_EXE = "D:\Software\miniforge3\Library\bin\mamba.exe"
 (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
 #endregion
