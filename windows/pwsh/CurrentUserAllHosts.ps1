9ddb7b023f7b85f2bdc0f8fd357337c6de89175d
wit-l <witty.ysl@gmail.com>
2025-10-12 13:32:15 +0800

    chore(pwsh): change file format

--- windows/pwsh/CurrentUserAllHosts.ps1
+++ windows/pwsh/CurrentUserAllHosts.ps1
@@ -1,16 +1,16 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-$PSStyle.FileInfo.Directory="`e[34;1m"
-starship init powershell | Invoke-Expression
-# 初始化zoxide
-zoxide init powershell | Out-String | Invoke-Expression
-# 初始化fnm
-fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
-#------------------------------- Set Hot-keys BEGIN -------------------------------
-# 设置预测文本来源为历史记录
-Set-PSReadLineOption -PredictionSource History -HistorySearchCursorMovesToEnd -EditMode Emacs -PredictionViewStyle InlineView -BellStyle None 
-Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
-#------------------------------- Set Hot-keys END -------------------------------
+$PSStyle.FileInfo.Directory="`e[34;1m"
+starship init powershell | Invoke-Expression
+# 初始化zoxide
+zoxide init powershell | Out-String | Invoke-Expression
+# 初始化fnm
+fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+#------------------------------- Set Hot-keys BEGIN -------------------------------
+# 设置预测文本来源为历史记录
+Set-PSReadLineOption -PredictionSource History -HistorySearchCursorMovesToEnd -EditMode Emacs -PredictionViewStyle InlineView -BellStyle None
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+#------------------------------- Set Hot-keys END -------------------------------
 #region mamba initialize
 # !! Contents within this block are managed by 'mamba shell init' !!
 $Env:MAMBA_ROOT_PREFIX = "D:\Software\miniforge3"
