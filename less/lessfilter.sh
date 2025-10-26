163065bc8fc94d741da8e7934ef9094527ac082d
wit-l <witty.ysl@gmail.com>
2025-10-27 01:22:19 +0800

    feat(fzf-preview): enable preview image with fzf in kitty

--- less/lessfilter.sh
+++ less/lessfilter.sh
@@ -7,17 +7,50 @@ file=${1/#\~\//$HOME/}
 if [ -d "$file" ]; then
   eza --git -ahl --color=always --icons "$file"
 elif [ "$category" = image ]; then
+  # dim=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}
+  # if [[ $dim = x ]]; then
+  #   dim=$(stty size </dev/tty | awk '{print $2 "x" $1}')
+  # elif ! [[ $KITTY_WINDOW_ID ]] && ((FZF_PREVIEW_TOP + FZF_PREVIEW_LINES == $(stty size </dev/tty | awk '{print $1}'))); then
+  #   # Avoid scrolling issue when the Sixel image touches the bottom of the screen
+  #   # * https://github.com/junegunn/fzf/issues/2544
+  #   dim=${FZF_PREVIEW_COLUMNS}x$((FZF_PREVIEW_LINES - 1))
+  # fi
+  # img2sixel "$1" -w 600 -h 400
+  # chafa "$file" -f sixels -s "$dim" --stretch --clear
+  # exiftool "$file"
+
   dim=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}
-  if [[ $dim = x ]]; then
+  if [[ $dim == x ]]; then
     dim=$(stty size </dev/tty | awk '{print $2 "x" $1}')
   elif ! [[ $KITTY_WINDOW_ID ]] && ((FZF_PREVIEW_TOP + FZF_PREVIEW_LINES == $(stty size </dev/tty | awk '{print $1}'))); then
     # Avoid scrolling issue when the Sixel image touches the bottom of the screen
     # * https://github.com/junegunn/fzf/issues/2544
     dim=${FZF_PREVIEW_COLUMNS}x$((FZF_PREVIEW_LINES - 1))
   fi
-  # img2sixel "$1" -w 600 -h 400
-  chafa "$file" -f sixels -s "$dim" --stretch --clear
-  exiftool "$file"
+
+  # 1. Use icat (from Kitty) if kitten is installed
+  if [[ $KITTY_WINDOW_ID ]] || [[ $GHOSTTY_RESOURCES_DIR ]] && command -v kitten >/dev/null; then
+    # 1. 'memory' is the fastest option but if you want the image to be scrollable,
+    #    you have to use 'stream'.
+    #
+    # 2. The last line of the output is the ANSI reset code without newline.
+    #    This confuses fzf and makes it render scroll offset indicator.
+    #    So we remove the last line and append the reset code to its previous line.
+    kitten icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place="$dim@0x0" "$file" | sed '$d' | sed $'$s/$/\e[m/'
+
+  # 2. Use chafa with Sixel output
+  elif command -v chafa >/dev/null; then
+    chafa -s "$dim" "$file"
+    # Add a new line character so that fzf can display multiple images in the preview window
+    echo
+
+  # 3. If chafa is not found but imgcat is available, use it on iTerm2
+  elif command -v imgcat >/dev/null; then
+    # NOTE: We should use https://iterm2.com/utilities/it2check to check if the
+    # user is running iTerm2. But for the sake of simplicity, we just assume
+    # that's the case here.
+    imgcat -W "${dim%%x*}" -H "${dim##*x}" "$file"
+  fi
 elif [ "$kind" = vnd.openxmlformats-officedocument.spreadsheetml.sheet ] ||
   [ "$kind" = vnd.ms-excel ]; then
   in2csv "$file" | xsv table | bat -n -ltsv --color=always
