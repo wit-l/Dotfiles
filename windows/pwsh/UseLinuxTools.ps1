8a5cd4110420edd36f23a1ccfeb3e467d0f47fb0
wit-l <witty.ysl@gmail.com>
2025-11-28 00:42:26 +0800

    update

--- windows/pwsh/UseLinuxTools.ps1
+++ windows/pwsh/UseLinuxTools.ps1
@@ -2,15 +2,15 @@ function EnableLinuxTools{
     $path=$ENV:PATH -split ';'
     $path=$path | Where-Object { $_ }
     $ENV:PATH=$path -join ';'
-    if ($path.IndexOf("D:\Software\Git\usr\bin") -eq -1) {
-        $ENV:PATH+=";D:\Software\Git\usr\bin"
+    if ($path.IndexOf("C:\Software\Git\usr\bin") -eq -1) {
+        $ENV:PATH+=";C:\Software\Git\usr\bin"
     }
 }
 
 function DisableLinuxTools{
     $str=$ENV:PATH
     $str=$str -split ';'
-    $str=$str | Where-Object { $_ -and ! ($_ -eq "D:\Software\Git\usr\bin") }
+    $str=$str | Where-Object { $_ -and ! ($_ -eq "C:\Software\Git\usr\bin") }
     $ENV:PATH=$str -join ';'
 }
 Set-Alias -Name "elt" -Value "EnableLinuxTools"
